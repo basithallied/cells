442cb36f134acb0d1fa9f204f4ce4dfa05f4cd5f
zinfog <shafeek@zinfog.com>
2022-12-05 19:42:59 +0530

    added report

--- cells_terms/models/purchase_order_inherit.py
+++ cells_terms/models/purchase_order_inherit.py
@@ -5,3 +5,7 @@ class PurchaseOrderInherit(models.Model):
     _inherit = 'purchase.order'
 
     terms_category_id = fields.Many2one("cells.terms.category", "Terms Category")
+    shipping_method = fields.Selection([
+        ('by_road', 'By Road'),
+        ('by_air', 'By Air'),
+        ('by_sea', 'By Sea')], string="Shipping Method", default='by_road')
