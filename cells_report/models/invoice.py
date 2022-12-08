223d5be0561a21959a97bed0a0f9339ea11eb3b3
Zinfog <mubaris@zinfog.com>
2022-12-08 12:56:42 +0530

    python file changed

--- cells_report/models/invoice.py
+++ cells_report/models/invoice.py
@@ -11,12 +11,9 @@ class AccountMove(models.Model):
             'invoice_id': invoice.id,
         }
         return self.env.ref('cells_report.action_cells_tax_invoice_report').report_action(self, data=data)
-    # def print_conditional_invoice_arabic(self):
-    #     invoice = self.env['account.move'].search([('id', '=', self.id)])
-    #     data = {
-    #         'invoice_id': invoice.id,
-    #     }
-    #     return self.env.ref('cells_report.action_report_invoice_arabic').report_action(self, data=data)
+    def print_conditional_invoice_arabic(self):
+   # merlin changed
+        return self.env.ref('cells_report.action_report_invoice_arabic').report_action(self)
 
     def print_vendor_bill(self):
         invoice = self.env['account.move'].search([('id', '=', self.id)])
